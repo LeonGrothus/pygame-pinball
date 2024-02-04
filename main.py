c4410e665d71c983e43ad59f63331b1f82a78af7
Leon Grothus <leon.grothus@gmail.com>
2024-02-04 19:11:01 +0100

    changed main layout

--- main.py
+++ main.py
@@ -10,30 +10,39 @@ This is the main file of the game. It initializes PyGame and creates the screen 
 The main event loop is also located here.
 """
 
-# Initialize PyGame
-pygame.init()
+def main():
+    """
+    The main function of the game. It initializes PyGame and creates the screen and clock. It also creates the sound manager and scene manager.
+    The main event loop is also located here.
+    """
 
-options = OptionsManager() # Load options
-screen = pygame.display.set_mode(options.resolution) # Create the screen
-clock = pygame.time.Clock() # Create clock
+    # Initialize PyGame
+    pygame.init()
 
-sound_manager = SoundManager() # Create sound manager
-sound_manager.set_options(options) # Set options
-sound_manager.load_music() # Load music
-sound_manager.play_music() # Play music
+    options = OptionsManager() # Load options
+    screen = pygame.display.set_mode(options.resolution) # Create the screen
+    clock = pygame.time.Clock() # Create clock
 
-scene_manager = SceneManager(screen, "main_menu") # Create scene manager and set default scene to main_menu
+    sound_manager = SoundManager() # Create sound manager
+    sound_manager.set_options(options) # Set options
+    sound_manager.load_music() # Load music
+    sound_manager.play_music() # Play music
 
-# Main event loop
-while True:
-    events = pygame.event.get() # Get all events
-    sound_manager.update(events) # Update sound manager
-    for event in events:
-        if event.type == pygame.QUIT:   
-            pygame.quit()
-            sys.exit()
-        
-    scene_manager.active_scene.update(constants.DELTA_TIME, events)  # Update the scene
+    scene_manager = SceneManager(screen, "main_menu") # Create scene manager and set default scene to main_menu
 
-    pygame.display.flip()  # Update the display
-    clock.tick(constants.FRAMERATE)  # Limit the framerate
+    # Main event loop
+    while True:
+        events = pygame.event.get() # Get all events
+        sound_manager.update(events) # Update sound manager
+        for event in events:
+            if event.type == pygame.QUIT:   
+                pygame.quit()
+                sys.exit()
+            
+        scene_manager.active_scene.update(constants.DELTA_TIME, events)  # Update the scene
+
+        pygame.display.flip()  # Update the display
+        clock.tick(constants.FRAMERATE)  # Limit the framerate
+
+if __name__ == "__main__":
+    main()  # Run the main function
