8cb9447eab39e08d06ef338b43cb6713998128e7
Leon Grothus <leon.grothus@gmail.com>
2024-02-04 17:36:12 +0100

    fixed background scaling

--- source/api/management/background_manager.py
+++ source/api/management/background_manager.py
@@ -30,13 +30,9 @@ class BackgroundManager:
             screen (pygame.Surface): The screen to draw the image on. 
         """
         # Load the images and scale them to the screen size
-        image_files = sorted([image for image in os.listdir(image_dir) if image.endswith('.jpg')])
-        self.images = []
-        for image_file in image_files:
-            image_path = os.path.join(image_dir, image_file)
-            pygame_image = pygame.image.load(image_path).convert()
-            self.images.append(pygame.transform.scale(pygame_image, (screen.get_width(), screen.get_height())))
+        self.load_images()
         
+        self.image_dir = image_dir
         self.delay = delay
         self.screen = screen
         self.current_image = 0
@@ -58,8 +54,14 @@ class BackgroundManager:
             self.current_image = (self.current_image + 1) % len(self.images)
             self.last_update = 0
 
-    def update_scale(self) -> None:
+    def load_images(self) -> None:
         """
-        Updates the scale of the images.
+        Loads the images from the specified directory and scales them to the screen size. 
+        Modifies the images in place.
         """
-        self.images = [pygame.transform.scale(image, (int(self.screen.get_width()), int(self.screen.get_height()))) for image in self.images]
+        image_files = sorted([image for image in os.listdir(self.image_dir) if image.endswith('.jpg')])
+        self.images = []
+        for image_file in image_files:
+            image_path = os.path.join(self.image_dir, image_file)
+            pygame_image = pygame.image.load(image_path).convert()
+            self.images.append(pygame.transform.scale(pygame_image, (self.screen.get_width(), self.screen.get_height())))
--- source/api/scene/base_display.py
+++ source/api/scene/base_display.py
@@ -40,7 +40,7 @@ class BaseDisplay(ABC):
         Returns:
             None
         """
-        self.background_manager.update_scale()
+        self.background_manager.load_images()
         pass
 
     def update(self, delta_time: float, events: list[Event]) -> None:
